34a6f1d997ef2201494057e2a76805b8bfb2a0ba
Momore <akinolamomoreoluwa@gmail.com>
2025-01-07 19:25:10 -0500

    We Started With The File Handling

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+            try {
+                File myObj = new File("Example.exe");
+                
+                
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+    }
+}
