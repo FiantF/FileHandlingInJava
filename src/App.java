546cb959624489647d518f6ffe8f3887b5ebed02
Momore <akinolamomoreoluwa@gmail.com>
2025-01-08 19:25:50 -0500

    Scanner Class

--- src/App.java
+++ src/App.java
@@ -1,9 +1,20 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
             try {
-                File myObj = new File("Example.exe");
+                File myObj = new File("Example.txt");
+                Scanner sc = new Scanner(myObj);
+
+                while (sc.hasNextLine() ){
+                    String data = sc.nextLine();
+                    System.out.println(data);
+                     
+                }
+
+                sc.close();
+
                 
                 
             } catch (Exception e) {
